8c6b75cbc1574a23e3b56bd03f7a2896d6c7b4bc
Abijith-V-001 <abivandiyil001@gmail.com>
2024-06-09 11:56:50 +0530

    Created page to search students

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddStudent from './Components/AddStudent';
+import Search from './Components/Search';
 
 function App() {
   return (
     <div>
       <AddStudent/>
+      <Search/>
     </div>
   );
 }
